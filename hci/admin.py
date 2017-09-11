0993741d4250426969a3a1b5727563ecacd91458
JuneBuug <junee613@gmail.com>
2017-09-11 11:36:08 +0900

    scoring 모델 추가

--- hci/admin.py
+++ hci/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CheerioSong,CheerioDance
+from .models import CheerioSong,CheerioDance,Scoring
 # Register your models here.
 
 admin.site.register(CheerioSong)
 admin.site.register(CheerioDance)
+admin.site.register(Scoring)
--- hci/models.py
+++ hci/models.py
@@ -17,3 +17,11 @@ class CheerioDance(models.Model) :
 
     def __str__(self):
         return self.title
+
+class Scoring(models.Model) :
+    year = models.CharField(max_length=20)
+    score = models.CharField(max_length=128)
+    etc = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.year
