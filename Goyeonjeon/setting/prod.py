399710a30d2870dfb038f9031cafb7dd4043383f
JuneBuug <junee613@gmail.com>
2017-09-15 11:40:17 +0900

    production settings 수정

--- Goyeonjeon/setting/prod.py
+++ Goyeonjeon/setting/prod.py
@@ -8,8 +8,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
 
 DATABASES = { 'default': {
-  'ENGINE': 'django.db.backends.postgresql', 'NAME': 'ubuntu',
+  'ENGINE': 'django.db.backends.sqlite3',
+  'NAME': 'ubuntu',
   'USER': 'ubuntu',
-  'PASSWORD': 'withaskdjango!',
+  'PASSWORD': '',
   'HOST': '127.0.0.1', },
 }
