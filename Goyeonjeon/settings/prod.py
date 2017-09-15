0210549613584ecaa02dfa595af7fe289cbc0e8d
JuneBuug <junee613@gmail.com>
2017-09-16 08:34:14 +0900

    ask django 배포에 따라 변경

--- Goyeonjeon/settings/prod.py
+++ Goyeonjeon/settings/prod.py
@@ -1,13 +1,4 @@
-try:
-    from .common import *
-    import os
-    from django.contrib.messages import constants as messages
-    MESSAGE_TAGS = {
-        messages.ERROR: 'danger',
-    }
-except ImportError:
-    pass
-
+from .dev import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['\*']
