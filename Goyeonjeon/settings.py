b018c636b9d095a1d4397592a68396c537c7cfde
JuneBuug <junee613@gmail.com>
2017-09-16 09:20:47 +0900

    되돌림

--- Goyeonjeon/settings.py
+++ Goyeonjeon/settings.py
@@ -121,14 +121,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-try:
-    from django.contrib.messages import constants as messages
-    MESSAGE_TAGS = {
-        messages.ERROR: 'danger',
-    }
-except ImportError:
-    pass
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- Goyeonjeon/wsgi.py
+++ Goyeonjeon/wsgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Goyeonjeon.settings")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Goyeonjeon.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Goyeonjeon.settings")
 application = get_wsgi_application()
